51a63257190d8ad75a8f07b2588d24df3f0c619a
Miolus <69853686+Miolus@users.noreply.github.com>
2022-10-01 12:21:19 +0200

    Update and rename localizations.json to localizations.py

---
+++ localizations.py
@@ -0,0 +1,137 @@
+"""
+The contents of this file may be used publicly in the InviteChecker app.
+© Miolus 2022 - present
+"""
+cmd_desc = {
+        "leaderboard": {
+            "de":"Zeige die Einladungs-Rangliste für deinen Server."
+            },
+        "g_start": {
+            "de":"Starte ein Giveaway."
+        },
+        "g_end": {
+            "de":"Beende das Giveaway."
+        },
+        "g_reroll": {
+            "de":"Lose erneut Gewinner aus."
+        },
+        "graph": {
+            "de":"Zeige Wachstumsstatistiken deiner Community."
+        },
+        "userinfo": {
+            "de":"Zeige Info über einen Nutzer."
+        },
+        "invites": {
+            "de":"Zeige, wie viele Einladungen ein Nutzer hat."
+            },
+        "c_add": {
+            "de":"Füge Bonus Einladungen einem Nutzer hinzu."
+        },
+        "c_remove": {
+            "de":"Entferne Bonus Einladungen von einem Nutzer."
+        },
+        "mylinks": {
+            "de":"Zeige deine Einladungs-Links."
+        },
+        "config": {
+            "de":"Öffne die Einstellungsseite."
+        },
+        "ch_join": {
+            "de":"Lege einen Kanal für Willkommensnachrichten fest."
+        },
+        "ch_leave": {
+            "de":"Lege einen Kanal für Verlassnachrichten fest."
+        },
+        "msg_join": {
+            "de":"Ändere die Willkommensnachricht."
+        },
+        "msg_leave": {
+            "de":"Ändere die Verlassnachricht."
+        },
+        "help": {
+            "de":"Das Hilfe-Menu."
+        },
+        "debug":{
+            "de":"Öffne das Debug-Menu."
+        },
+        "g_delete": {
+            "de":"Lösche eine Verlosung."
+        },
+        "update": {
+            "de":"Aktualisiere den Einladungs-Index."
+        },
+        "g_wipe": {
+            "de":"Lösche alte Verlosungen für die Übersicht."
+        },
+        "g_list": {
+            "de":"Zeige alle Verlosungen an."
+        },
+}
+options_desc =  {
+        "user": {
+            "en-US":"Choose an user.",
+            "de":"Wähle einen Nutzer."
+        },
+        "filter": {
+            "en-US":"Set an output filter.",
+            "de":"Wähle einen Filter."
+        },
+        "page": {
+            "en-US":"Select a page to view.",
+            "de":"Wähle eine Seite."
+        },
+        "count": {
+            "en-US":"Set the count of invites to change.",
+            "de":"Die Anzahl der Einladunge."
+        },
+        "msg": {
+            "en-US":"Set the new message for this event.",
+            "de":"Die neue Nachricht, die für dieses Event gespeichert werden soll."
+        },
+        "channel": {
+            "en-US":"Set a channel.",
+            "de":"Der Kanal, der festgelegt werden soll."
+        },
+        "style":{
+            "en-US":"Change the style.",
+            "de":"Lege einen Stil fest."
+        },
+        "id":{
+            "en-US":"The giveaway id. Get it from /giveaway list.",
+            "de":"Die Verlosungs-ID. Bekomme sie von dem /giveaway list Befehl."
+        }
+    }
+options_name =  {
+        "user": {
+            "en-US":"user",
+            "de":"nutzer"
+        },
+        "filter": {
+            "en-US":"filter",
+            "de":"filter"
+        },
+        "page": {
+            "en-US":"page",
+            "de":"seite"
+        },
+        "count": {
+            "en-US":"count",
+            "de":"anzahl"
+        },
+        "msg": {
+            "en-US":"msg",
+            "de":"nachricht"
+        },
+        "channel": {
+            "en-US":"channel",
+            "de":"kanal"
+        },
+        "style": {
+            "en-US":"style",
+            "de":"design"
+        },
+        "id":{
+            "en-US":"id",
+            "de":"id"
+        }
+    }
